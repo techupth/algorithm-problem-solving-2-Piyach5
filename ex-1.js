27c608f78f09d9eedc6bfda9a68de5f1c86d1c39
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-07-17 14:27:37 +0700

    feature: search student and book

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,27 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  const result = students.indexOf(searchStudent);
+  return result;
 }
 
+const students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+
+const searchStudent = "John";
+
+console.log(findStudentIndex(students, searchStudent));
+
 // ตอบคำถามตรงนี้จ้า
+
+/* จากฟังค์ชั่น findStudentIndex ด้านบน BIG O จะมีค่าเท่ากับ 1 
+เนื่องจากไม่ว่าข้อมูลจะมีขนาดเท่าไหร่ แต่ function
+จะ execute เพียงหนึ่งครั้งในการหา index ของ searchStudent ด้วย indexOf()
+ที่เป็น built-in function*/
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,25 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let result = -1;
+  for (let i in books) {
+    if (books[i].title === searchTitle) {
+      result = i;
+    }
+  }
+  return result;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+const searchBook = "Gone with the Wind";
+
+console.log(findBookIndex(books, searchBook));
